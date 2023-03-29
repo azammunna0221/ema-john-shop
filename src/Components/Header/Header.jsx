dbd5a159401d3121b47ffecc1ce892961ebccef3
Ali Azam <azammunna0221>
2023-03-30 00:35:08 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <div>
+                    <p>hello</p>
+                </div>
+                <div>
+
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
